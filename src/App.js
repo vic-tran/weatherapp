46dc2ec8abcf324b503f2cd0245a3e101d035905
Victor Tran <vc.tran998@gmail.com>
2022-05-22 11:55:56 -0700

    Add openweathermap api key

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=4c292201046ae4fb1c8afc4eb45c9aba`
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
