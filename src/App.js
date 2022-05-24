3fd95e06a6660b67e0f4a0c9d259f3cb88b02c28
Victor Tran <vc.tran998@gmail.com>
2022-05-24 09:46:38 -0700

    Add city search input, information is displayed in console

--- src/App.js
+++ src/App.js
@@ -4,10 +4,31 @@ import axios from 'axios';
 
 function App() {
 
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=4c292201046ae4fb1c8afc4eb45c9aba`
+  const [data,setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=4c292201046ae4fb1c8afc4eb45c9aba`
+
+  const searchLocation = (event) => {
+    if (event.key == 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
   
   return (
     <div className="app">
+      <div className="search">
+        <input type="text" 
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter location"
+        />
+      </div>
       <div className="container">
 
         <div className="top">
@@ -24,13 +45,16 @@ function App() {
 
         <div className="bottom">
           <div className="feels">
-            <p>65°F</p>
+            <p className='bold'>65°F</p>
+            <p>Feels Like</p>
           </div>
           <div className="humidity">
-            <p>20%</p>
+            <p className='bold'>20%</p>
+            <p>Humidity</p>
           </div>
           <div className="wind">
-            12 MPH
+            <p className='bold'>12 MPH</p>
+            <p>Wind Speed</p>
           </div>
         </div>
         
