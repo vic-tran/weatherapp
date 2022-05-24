67a250fa61ad7aa182067d16739aa65e0bacd894
Victor Tran <vc.tran998@gmail.com>
2022-05-23 23:41:17 -0700

    Add background image and top and bottom containers with weather components. Also add style to components.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,37 @@ import axios from 'axios';
 
 function App() {
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=4c292201046ae4fb1c8afc4eb45c9aba`
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=4c292201046ae4fb1c8afc4eb45c9aba`
   
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <div className="container">
+
+        <div className="top">
+          <div className="location">
+          <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="description">
+            <p>Cloudly</p>
+          </div>
+        </div>
+
+        <div className="bottom">
+          <div className="feels">
+            <p>65°F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            12 MPH
+          </div>
+        </div>
+        
+      </div>
     </div>
   );
 }
